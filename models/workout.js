4becffb956c09f4a68ff6b3e9cbf405473e734ac
Arne Tripolone <arnetripolone@gmail.com>
2021-09-30 11:55:02 +1000

    Updates to workout.js model

--- models/workout.js
+++ models/workout.js
@@ -1,5 +1,46 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const workoutSchema = new Schema({
+  exercise: [
+    {
+      type: {
+        type: String,
+        required: 'Exercise type',
+      },
+      name: {
+        type: String,
+        required: 'Exercise name',
+      },
+      duration: {
+        type: Number,
+        required: 'Exercise time',
+      },
+      distance: {
+        type: Number
+      },
+      weight: {
+        type: Number
+      },
+      sets: {
+        type: Number
+      },
+      reps: {
+        type: Number
+      },
+    },
+  ],
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+module.exports = Workout;
+
+/* 
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
@@ -19,5 +60,5 @@ const workoutSchema = new Schema({
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
-
-module.exports = Workout;
+module.exports = Workout; 
+*/
