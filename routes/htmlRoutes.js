5cca06f2bea15d446aa5c9ed5072758c5fa44407
Arne Tripolone <arnetripolone@gmail.com>
2021-09-30 16:24:12 +1000

    Added exercise and stats page routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,9 +2,20 @@
 const path = require("path");
 const router = require("express").Router();
 
-//display homepage
+//Homepage
 router.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
+//Exercise
+router.get("/exercise", (req,res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+//Stats
+router.get("/stats", (req,res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
 module.exports = router
+
